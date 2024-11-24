25ec454ba4f5d3201f4e20bf002c485e4fd4f2fd
web2ls <web2ls@yandex.ru>
2024-11-24 18:25:51 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const LOCAL_PORT = 80;
+const REDIRECT_PORT = 6285;
+
+app.get('*', (request, response) => {
+  const url = `${request.protocol}://${request.hostname}:${REDIRECT_PORT}${request.originalUrl}`;
+  response.redirect(301, url);
+})
+
+app.listen(LOCAL_PORT, () => {
+  console.log('server has been started');
+})
